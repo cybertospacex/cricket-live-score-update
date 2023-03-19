4f4d6aed8fa734cd4cea584e406d8da262befc80
cybertospacex <128274576+cybertospacex@users.noreply.github.com>
2023-03-19 10:18:35 +0530

    Add files via upload

---
+++ src/components/ui/RadioGroupBoxStyle.js
@@ -0,0 +1,11 @@
+export const radioGroupBoxstyle = {
+  position: 'absolute',
+  top: '61%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 340,
+  bgcolor: 'background.paper',
+  border: '2px solid #6537b0',
+  boxShadow: 24,
+  p: '4px',
+}
